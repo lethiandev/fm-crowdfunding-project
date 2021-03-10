5003ac101ac412c6b33bb440b86db1d8e73bc16e
Konrad Nowakowski <konrad.x92@gmail.com>
2021-03-10 15:16:07 +0000

    Remove buttons rendering

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,6 @@
   <TheHeader>
     <TheNavbar></TheNavbar>
   </TheHeader>
-  <BaseButton>
-    Back this project
-  </BaseButton>
-  <BaseButtonIcon :icon="IconBookmark">
-    Bookmark
-  </BaseButtonIcon>
 </template>
 
 <script lang="ts">
@@ -15,16 +9,12 @@ import { defineComponent } from 'vue';
 import IconBookmark from './assets/icon-bookmark.svg';
 import TheHeader from './components/TheHeader.vue';
 import TheNavbar from './components/TheNavbar.vue';
-import BaseButton from './components/BaseButton.vue';
-import BaseButtonIcon from './components/BaseButtonIcon.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
     TheHeader,
     TheNavbar,
-    BaseButton,
-    BaseButtonIcon,
   },
   computed: {
     IconBookmark: () => IconBookmark,
