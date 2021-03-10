df01250867a60f3be8965d2e21222093e576544a
Konrad Nowakowski <konrad.x92@gmail.com>
2021-03-10 17:44:04 +0000

    Add dropdown styling on mobile devices

--- src/components/NavbarListItem.vue
+++ src/components/NavbarListItem.vue
@@ -13,17 +13,37 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
+@use '~@/scss/variables' as vars;
+
 .navbar-list-item {
   margin-left: 32px;
+  line-height: 1em;
+  display: block;
+
+  @media (max-width: vars.$breakpoint-mobile) {
+    margin: 0;
+    padding: 0;
+  }
 }
 
 .navbar-list-item::v-deep > a {
   text-decoration: none;
   color: white;
+  display: block;
 
   &:hover,
   &:active {
     text-decoration: underline;
+  }
+
+  @media (max-width: vars.$breakpoint-mobile) {
+    padding: 27px 24px;
+    font-weight: 500;
+    color: black;
+
+    & {
+      border-bottom: solid 1px #ddd;
+    }
   }
 }
 </style>
