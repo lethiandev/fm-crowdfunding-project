06ce76e4a956683c72ed81a78f87a9f54dd37c3b
Konrad Nowakowski <konrad.x92@gmail.com>
2021-03-10 00:51:14 +0000

    Add base button with icon component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,26 @@
+<template>
+  <BaseButton>
+    Back this project
+  </BaseButton>
+  <BaseButtonIcon :icon="IconBookmark">
+    Bookmark
+  </BaseButtonIcon>
+</template>
+
 <script lang="ts">
 import { defineComponent } from 'vue';
+import IconBookmark from './assets/icon-bookmark.svg';
+import BaseButton from './components/BaseButton.vue';
+import BaseButtonIcon from './components/BaseButtonIcon.vue';
 
 export default defineComponent({
   name: 'App',
+  components: {
+    BaseButton,
+    BaseButtonIcon,
+  },
+  computed: {
+    IconBookmark: () => IconBookmark,
+  },
 });
 </script>
---
+++ src/components/BaseButtonIcon.vue
@@ -0,0 +1,63 @@
+<template>
+  <BaseButton class="button-icon">
+    <div class="icon-wrapper">
+      <BaseIcon :icon="icon" />
+    </div>
+    <slot></slot>
+  </BaseButton>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import BaseButton from './BaseButton.vue';
+import BaseIcon from './BaseIcon.vue';
+
+export default defineComponent({
+  name: 'BaseButtonIcon',
+  components: {
+    BaseButton,
+    BaseIcon,
+  },
+  props: {
+    icon: {
+      type: String,
+      required: true,
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+@use '~@/scss/variables' as vars;
+
+.button-icon {
+  $padding-y: nth($list: vars.$button-padding, $n: 1);
+  border-left: solid calc(#{$padding-y} + 0.5em) transparent;
+  position: relative;
+
+  // Add left margin to content
+  &::before {
+    content: '';
+    margin-left: 4px;
+    visibility: hidden;
+  }
+}
+
+.icon-wrapper {
+  background-color: #2F2F2F;
+  border-radius: 2048px;
+  height: 100%;
+
+  // Pull the icon to left edge
+  transform: translate(-50%, 0);
+  position: absolute;
+  top: 0;
+  left: 0;
+
+  > .icon {
+    pointer-events: none;
+    user-select: none;
+    height: 100%;
+  }
+}
+</style>
