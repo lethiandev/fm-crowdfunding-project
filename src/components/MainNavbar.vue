58319f5742a2dd344d6893be7710d72115305103
Konrad Nowakowski <konrad.x92@gmail.com>
2021-03-11 13:33:37 +0000

    Fix main navbar component template complexity

--- src/components/MainNavbar.vue
+++ src/components/MainNavbar.vue
@@ -1,12 +1,10 @@
 <template>
-  <div class="navbar-wrapper">
+  <div class="navbar">
     <div class="container">
-      <nav class="navbar">
-        <a class="logo" href="#">
-          <img src="~@/assets/logo.svg" alt="Crowdfund" />
-        </a>
-        <slot></slot>
-      </nav>
+      <a class="brand" href="/">
+        <img class="logo" src="~@/assets/logo.svg" alt="Crowdfund project logo" />
+      </a>
+      <slot></slot>
     </div>
   </div>
 </template>
@@ -22,28 +20,28 @@ export default defineComponent({
 <style lang="scss" scoped>
 @use '~@/scss/variables' as vars;
 
-.navbar-wrapper {
-  background-image: linear-gradient(to bottom, rgba(0,0,0,.5), rgba(0,0,0,0));
-}
-
 .navbar {
   padding: 48px 0;
-  display: flex;
-  align-items: center;
-  position: relative;
+  background-image: linear-gradient(
+    to bottom, rgba(0,0,0,.5), transparent );
 
   @media (max-width: vars.$breakpoint-mobile) {
-    padding: 32px 0 36px 0;
+    padding: 32px 0;
   }
+}
+
+.container {
+  display: flex;
+  align-items: center;
+}
+
+.brand {
+  margin-right: auto;
+  display: block;
 }
 
 .logo {
-  margin-right: auto;
   display: block;
-
-  img {
-    max-width: 128px;
-    display: block;
-  }
+  max-width: 128px;
 }
 </style>
